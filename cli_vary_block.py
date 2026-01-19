e790fdde4da443210494e9d90d8d9689262051de
Thach <Thachvu1127@gmail.com>
2026-01-19 11:24:07 -0800

    add the proper imports, logic for removing autoset and adding angle and/or flip_x property, save changes via playdo.Write()

--- cli_vary_block.py
+++ cli_vary_block.py
@@ -23,10 +23,14 @@ Pseudo Algo
 
 import argparse
 import logic.common.level_playdo as play
+import logic.common.tiled_utils as tiled
+import logic.common.file_utils as file_utils
+import random
 
 
 arg_desription = "Scans a level file for relic blocks and apply the custom properties of flip_x and angle"
 arg_help1 = "Name of the tiled level XML"
+angle = [0, 90, 180, 270]
 
 def GetAllRelicBlocks(playdo):
     obj_grps = playdo.GetAllObjectgroup(is_print=False)
@@ -44,6 +48,7 @@ def GetAllRelicBlocks(playdo):
         for shape in obj_grp:
             if shape.get("name") == "relic_block":
                 relic_blocks.append(shape)
+    print(f"There are {len(relic_blocks)} relic blocks")
     return relic_blocks
 
 def main():
@@ -52,13 +57,18 @@ def main():
     args = parser.parse_args()
     print(f"Running for cli_vary_block on Tiled level {args.filename}")
 
-    playdo = play.LevelPlayDo(args.filename)
+    playdo = play.LevelPlayDo(file_utils.GetFullLevelPath(args.filename))
     relic_blocks = GetAllRelicBlocks(playdo)
 
     if not relic_blocks:
         print(f"No relic blocks found inside Tiled level {args.filename}")
         return;
     
+    for relic_block in relic_blocks:
+        tiled.RemovePropertyFromObject(relic_block, "autoset")
+        tiled.SetPropertyOnObject(relic_block, "angle", str(random.choice(angle)))
+        if random.choice([True, False]):
+            tiled.SetPropertyOnObject(relic_block, "flip_x", "true")
 
-
+    playdo.Write()
 main();
