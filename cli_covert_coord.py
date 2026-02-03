341054536f749deecfa5e7cdf57aa7dcb1dc74af
Thach <Thachvu1127@gmail.com>
2026-02-03 00:48:51 -0800

    create ConvertToPoint() which takes old coordinate  attributes and connvert it over to a new coordinate for point object

--- cli_covert_coord.py
+++ cli_covert_coord.py
@@ -12,29 +12,45 @@ import argparse
 import logic.common.file_utils as file_utils
 import logic.common.level_playdo as play
 import logic.common.tiled_utils as tiled
-
+import xml.etree.ElementTree as ET
 
 
 tool_description = "Scans a level for original coord objects and convert these objects into point objects"
 arg_help_level = "Named of the tiled level XML"
 
 
-def FindCenter(x, y, w, h):
-    center_x = x + (w // 2)
-    center_y = y + (h // 2)
-    return (x, y)
+def ConvertToPoint(coord, index):
+    # get x, y, width, height values from original coord
+    x = int(coord.get('x'))
+    y = int(coord.get('y'))
+    width = int(coord.get('width'))
+    height = int(coord.get('height'))
+
+    # calculate new x,y coordinates for point
+    center_x = x + (width // 2)
+    center_y = y + (height // 2)
+
+    # update coordinates
+    coord.set('x', str(center_x))
+    coord.set('y', str(center_y))
+    coord.set('name', str(f"k{index}"))
+    coord.set('type', "coord")
+
+    # remove the old custom property, since we are renaming the new point object (k1, k2, k3 etc...)
+    tiled.RemovePropertyFromObject(coord, 'ref')
+
+    # delete width and height XML attribute
+    del coord.attrib['width']
+    del coord.attrib['height']
+
+    ET.SubElement(coord, 'point')
 
 def ReplaceCoord(filename):
     playdo = play.LevelPlayDo(file_utils.GetFullLevelPath(filename))
     coords = playdo.GetAllObjectsWithName("coord")
-    for coord in coords:
-        x = int(coord.get('x'))
-        y = int(coord.get('y'))
-        width = int(coord.get('width'))
-        height = int(coord.get('height'))
-        new_x, new_y = FindCenter(x, y, width, height)
-        print(new_x)
-        print(new_y)
+    for num, coord in enumerate(coords):
+       ConvertToPoint(coord, num + 1)
+    playdo.Write()
 
 
 def main():
